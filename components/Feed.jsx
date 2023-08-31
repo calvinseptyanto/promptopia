e9c83533272277d2073f6944900998f79fbbbf10
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-08-31 22:42:06 +0800

    Finished searching function

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,18 +18,50 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
-  const handleSearchChange = (e) => {};
+  const [allPosts, setAllPosts] = useState([]);
 
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
-      setPosts(data);
+      setAllPosts(data);
     };
     fetchPosts();
   }, []);
+
+  const filterPrompts = (textFilter) => {
+    const regex = new RegExp(textFilter, "i");
+    return allPosts.filter(
+      (post) =>
+        regex.test(post.prompt) ||
+        regex.test(post.tag) ||
+        regex.test(post.creator.username) ||
+        regex.test(post.creator.email)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tag) => {
+    setSearchText(tag);
+    const searchResult = filterPrompts(tag);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -42,7 +74,14 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
